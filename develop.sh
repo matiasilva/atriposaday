8191e93b32bce201427e68e99a0cc6213ede6582
Matias Silva <mw781@srcf.net>
2021-02-21 19:47:17 +0000

    Update develop script

--- develop.sh
+++ develop.sh
@@ -7,36 +7,25 @@ scrap() {
 
 build() {(
   set -e
-  cp .sample-env .env
-  docker-compose -p development -f docker/development.yml up -d
-  pipenv install --dev
-  pipenv run -- flask db init
-  pipenv run -- flask db migrate -m 'First migration'
-  pipenv run -- flask db upgrade
-  docker-compose -p development -f docker/development.yml stop
+  docker-compose -p development -f docker-compose.yml up -d
+  npx sequelize-cli db:migrate
+  docker-compose -p development -f docker-compose.yml stop
 )}
 
 stop() {
-  docker-compose -p development -f docker/development.yml down
+  docker-compose -p development -f docker-compose.yml down
 }
-
-reset(){(
-  docker volume rm development_pgdata
-  docker-compose -p development -f docker/development.yml up -d
-  pipenv run -- flask db upgrade
-  docker-compose -p development -f docker/development.yml stop
-)}
 
 stop_() {
   stop >/dev/null 2>&1
 }
 
 start() {
-  docker-compose -p development -f docker/development.yml up -d
+  docker-compose -p development -f docker-compose.yml up -d
 }
 
 tail() {
-  docker-compose -p development -f docker/development.yml up
+  docker-compose -p development -f docker-compose.yml up
 }
 
 db() {
@@ -55,7 +44,6 @@ Verbs:
   stop     stop a daemon
   restart  restart a daemon (= stop start)
   scrap    flush all related docker and db state
-  reset    wipe the entire database but keep schema
   db       open a bash session in the db container
   rebuild  rebuild in case of schema changes (= scrap build)
 
@@ -81,7 +69,6 @@ for verb in "$@"; do
     scrap) stop_; scrap;;
     start) stop_; start;;
     restart) stop_; start;;
-    reset) stop_; reset;;
     db) db;;
     stop) stop;;
     tail) stop_; tail;;
