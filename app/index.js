c0a79df0bdc956f889d573aa263505af5f737af2
Matias Silva <mw781@srcf.net>
2021-02-22 21:58:04 +0000

    Add handlers and new routes

--- app/index.js
+++ app/index.js
@@ -1,13 +1,50 @@
 const express = require('express');
 const app = express();
-const db =  require('./models')
+const exphbs = require('express-handlebars');
+const db = require('./models')
 const PORT = 8080;
+
+const hbs = exphbs.create({
+    helpers: {
+        "is_admin_page": function (viewName, opts) {
+            if (viewName === "admin") {
+                return opts.fn(this);
+            }
+        }
+    }
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', 'app/views')
+
+// ideally only for dev
+app.use(express.static("app/public/"));
 
 // nb. db oject contains all models, the "sequelize" obj as the db conn, and Sequelize as tools
 
 app.get('/', (req, res) => {
-    res.status(200).send("Hello!");
+    res.render("home", {
+        title: "Home"
+    });
 })
+
+app.get('/admin', (req, res) => {
+    const { Topic } = db;
+    let rootTopics = Topic.findAll({
+        where: {
+            subLevel: 0
+        }
+    });
+    res.render("admin", {
+        title: "Administrator panel",
+        "root_topics": rootTopics
+    });
+})
+
+app.post('/admin/create/question', (req, res) => {
+
+});
 
 async function main() {
     try {
