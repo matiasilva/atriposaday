9cc8e543ba39f11f5187d8f785a1e8783a77b600
Matias Silva <mw781@srcf.net>
2021-02-23 23:57:05 +0000

    Add server-side logic for file upload

--- app/index.js
+++ app/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const db = require('./models')
+const { TRIPOS_PARTS } = require("./enums");
+const session = require('express-session');
+const flash = require('connect-flash');
+const upload = require('./middleware/upload');
+
 const PORT = 8080;
 
 const hbs = exphbs.create({
@@ -10,7 +15,7 @@ const hbs = exphbs.create({
             if (viewName === "admin") {
                 return opts.fn(this);
             }
-        }
+        },
     }
 });
 
@@ -20,7 +25,12 @@ app.set('views', 'app/views')
 
 // ideally only for dev
 app.use(express.static("app/public/"));
-
+app.use(session({
+	secret:'some secret',
+	saveUninitialized: true,
+	resave: true
+}));app.use(flash());
+    
 // nb. db oject contains all models, the "sequelize" obj as the db conn, and Sequelize as tools
 
 app.get('/', (req, res) => {
@@ -39,12 +49,39 @@ app.get('/admin', async (req, res) => {
     });
     res.render("admin", {
         title: "Administrator panel",
-        "root_topics": rootTopics
+        "root_topics": rootTopics,
+        "tripos_parts": Object.entries(TRIPOS_PARTS)
     });
 })
 
-app.post('/admin/create/question', (req, res) => {
-    console.log(req);
+app.post('/admin/create/question', upload.array('question-upload'), async (req, res) => {
+    let errors = {};
+
+    if (!req.files && req.files.length < 1) errors["question-upload"] = "You must upload at least one file."
+
+    if (req.body["tripos-part"] === "") errors["tripos-part"] = "You must specify a part of the Tripos to which your upload belongs."
+
+    if (req.body["description"].length >= 120) errors["description"] = "Your description is greater than 120 characters."
+
+    if (!errors) {
+        
+    }
+    else {
+        const { Topic } = db;
+        let rootTopics = await Topic.findAll({
+            where: {
+                isRootLevel: true
+            },
+            raw: true
+        });
+        // include sent back responses that failed
+        res.render("admin", {
+            title: "Administrator panel",
+            "root_topics": rootTopics,
+            "tripos_parts": Object.entries(TRIPOS_PARTS)
+        });
+
+    }
 });
 
 async function main() {
---
+++ app/middleware/upload.js
@@ -0,0 +1,22 @@
+const multer = require("multer");
+const nanoid = require("nanoid");
+const mime = require("mime-types");
+
+const imageFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb("Please upload only images.", false);
+  }
+};
+
+let storage = multer.diskStorage({
+  destination: "static/uploads",
+  filename: (req, file, cb) => {
+    cb(null, `${nanoid()}.${mime.extension(file.mimetype)}`);
+  },
+});
+
+let upload = multer({ storage, fileFilter: imageFilter });
+
+module.exports = upload;
