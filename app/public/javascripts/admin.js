545a4c21986e5716442f4850d8a68f6c2b90f4a5
Matias Silva <mw781@srcf.net>
2021-02-23 20:52:09 +0000

    Revert to sans serif, remove dropzone

--- app/public/javascripts/admin.js
+++ app/public/javascripts/admin.js
@@ -1,17 +1,17 @@
-window.Dropzone.options.questionCreate = {
-    maxFilesize: 4,
-    acceptedFiles: "image/*",
-    maxFiles: 1,
-    url: "/admin/create/question",
-    autoProcessQueue: false,
-    init: function () {
-        const myDropzone = this;
-        this.element.querySelector("button[type=submit]").addEventListener("click", function(e) {
-          // Make sure that the form isn't actually being sent.
-          e.preventDefault();
-          e.stopPropagation();
-          myDropzone.processQueue();
-        });
+// window.Dropzone.options.questionCreate = {
+//     maxFilesize: 4,
+//     acceptedFiles: "image/*",
+//     maxFiles: 1,
+//     url: "/admin/create/question",
+//     autoProcessQueue: false,
+//     init: function () {
+//         const myDropzone = this;
+//         this.element.querySelector("button[type=submit]").addEventListener("click", function(e) {
+//           // Make sure that the form isn't actually being sent.
+//           e.preventDefault();
+//           e.stopPropagation();
+//           myDropzone.processQueue();
+//         });
 
-    }
-};
+//     }
+// };
