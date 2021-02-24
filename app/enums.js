7e827163bb02e33d0974edf1a6b7e889927d078d
Matias Silva <mw781@srcf.net>
2021-02-24 10:13:58 +0000

    Reorganize enums with more Tripos info

--- app/enums.js
+++ app/enums.js
@@ -1,10 +1,12 @@
 module.exports = {
-    PAPERS: {
-        "2P1_MECHANICS": "2P1 Mechanics",
-        "2P2_STRUCTURES": "2P2 Structures",
-        "2P3_MATERIALS": "2P3 Materials",
-        "2P4_THERMOFLUID_MECHANICS": "2P4 Thermofluid Mechanics", "2P5_ELECTRICAL_ENGINEERING": "2P5 Electrical Engineering", "2P6_INFORMATION_ENGINEERING": "2P6 Information Engineering", "2P7_MATHEMATICAL_METHODS": "2P7 Mathematical Methods",
-        "2P8_SELECTED_TOPICS": "2P8 Selected Topics"
+    SUBJECTS: {
+        "IB": {
+            "2P1_MECHANICS": "2P1 Mechanics",
+            "2P2_STRUCTURES": "2P2 Structures",
+            "2P3_MATERIALS": "2P3 Materials",
+            "2P4_THERMOFLUID_MECHANICS": "2P4 Thermofluid Mechanics", "2P5_ELECTRICAL_ENGINEERING": "2P5 Electrical Engineering", "2P6_INFORMATION_ENGINEERING": "2P6 Information Engineering", "2P7_MATHEMATICAL_METHODS": "2P7 Mathematical Methods",
+            "2P8_SELECTED_TOPICS": "2P8 Selected Topics"
+        }
     },
     // "db_value": "label"
     TRIPOS_PARTS: {
@@ -12,5 +14,10 @@ module.exports = {
         "IB": "IB",
         "IIA": "IIA",
         "IIB": "IIB"
-    }
+    },
+    PAPER_TYPES: {
+        "EXAM": "Exam",
+        "EXAMPLES_PAPER": "Examples Paper"
+    },
+
 }
