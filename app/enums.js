3ffbb6b2ca3d60e51feb9a50c4c4f1c4c84bf8e3
Matias Silva <mw781@srcf.net>
2021-02-26 11:53:15 +0000

    Formatting on enums

--- app/enums.js
+++ app/enums.js
@@ -4,14 +4,17 @@ module.exports = {
             "2P1_MECHANICS": "2P1 Mechanics",
             "2P2_STRUCTURES": "2P2 Structures",
             "2P3_MATERIALS": "2P3 Materials",
-            "2P4_THERMOFLUID_MECHANICS": "2P4 Thermofluid Mechanics", "2P5_ELECTRICAL_ENGINEERING": "2P5 Electrical Engineering", "2P6_INFORMATION_ENGINEERING": "2P6 Information Engineering", "2P7_MATHEMATICAL_METHODS": "2P7 Mathematical Methods",
+            "2P4_THERMOFLUID_MECHANICS": "2P4 Thermofluid Mechanics",
+            "2P5_ELECTRICAL_ENGINEERING": "2P5 Electrical Engineering",
+            "2P6_INFORMATION_ENGINEERING": "2P6 Information Engineering",
+            "2P7_MATHEMATICAL_METHODS": "2P7 Mathematical Methods",
             "2P8_SELECTED_TOPICS": "2P8 Selected Topics"
         },
-        "IA":{
+        "IA": {
             "1P1_MECHANICS": "1P1 Mechanics",
             "1P2_STRUCTURES": "1P2 Structures",
             "1P3_ELECTRICAL_ENGINEERING": "1P3 Electrical Engineering",
-            "1P4_MATHEMATICAL_METHODS": "1P4 Mathematical Methods", 
+            "1P4_MATHEMATICAL_METHODS": "1P4 Mathematical Methods",
         }
     },
     // "db_value": "label"
