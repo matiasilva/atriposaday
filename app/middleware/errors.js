816ceb7309698690dfe8db9b2893281cc9f736b7
Matias Silva <mw781@srcf.net>
2021-02-26 17:18:07 +0000

    Improve error page

--- app/middleware/errors.js
+++ app/middleware/errors.js
@@ -3,7 +3,7 @@ module.exports = {
         res.status(404).render("404");
     },
     errorHandler: function (err, req, res, next) {
-        console.error(err);
+        console.error(err.stack);
         res.render("error", { error: err });
     }
 }
