f1e52423dc23aac0811d9930299979ea6ed0f605
Matias Silva <mw781@srcf.net>
2021-02-27 17:14:14 +0000

    Initialize sessions databae

--- app/server.js
+++ app/server.js
@@ -71,10 +71,11 @@ async function main() {
         console.log('Connection has been established successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error.name);
+        process.exit(1);
     }
 
-
     if (env === 'production') {
+        SequelizeStore.sync();
         app.listen(`./${process.env.ATAD_SOCKET}`, () => console.log("ATAD deployment started"));
     } else {
         app.listen(PORT, () => console.log(`We're live on ${PORT}!`));
