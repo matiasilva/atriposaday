538df4659417975ba3fc2798f41918fa5d812933
Matias Silva <mw781@srcf.net>
2021-02-27 11:40:03 +0000

    Make sure paper year is valid when adding question

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,7 +3,7 @@ const { TRIPOS_PARTS, SUBJECTS } = require("../enums");
 const upload = require('../middleware/upload');
 const utils = require('../utils');
 const db = require('../models');
-const {exposeUserInView} = require('../middleware/custom');
+const { exposeUserInView } = require('../middleware/custom');
 
 const router = express.Router();
 
@@ -29,6 +29,7 @@ router.get('/', async (req, res) => {
 
 router.post('/create/question', upload.array('question-upload'), async (req, res) => {
 
+    const { Answerable, Paper, Topic } = db;
     const formKeys = ["tripos-part", "description", "subject", "year"];
     const values = utils.pick(formKeys, req.body);
     let errors = {};
@@ -41,21 +42,25 @@ router.post('/create/question', upload.array('question-upload'), async (req, res
 
     if (values["description"].length >= 120) errors["description"] = true;
 
-    if (isNaN(Date.parse(values["year"]))) errors["year"] = true;
+    if (utils.isValidYear(values["year"])) errors["year"] = true;
+
+    const paper = await Paper.findOne({
+        where: {
+            year: parseInt(values["year"]),
+            subject: values["subject"],
+            triposPart: values["tripos-part"]
+        }
+    });
+
+    if (paper == null) {
+        req.flash("danger", "Failed to find a valid paper with these parameters");
+        res.redirect('/admin');
+    }
 
     const hasNoErrors = Object.keys(errors).length === 0;
+
     if (hasNoErrors) {
         // add question
-        const { Answerable, Paper, Topic } = db;
-
-        const paper = await Paper.findOne({
-            where: {
-                year: parseInt(values["year"]),
-                subject: values["subject"],
-                triposPart: values["tripos-part"]
-            }
-        });
-
         const topic = await Topic.findOne({
             where: {
                 isRootLevel: true,
