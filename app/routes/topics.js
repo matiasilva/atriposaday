8bf40768ec0dff24829bcf72ead84d8a786c7da7
Matias Silva <mw781@srcf.net>
2021-02-27 00:18:11 +0000

    Complete topic routes

--- app/routes/topics.js
+++ app/routes/topics.js
@@ -7,16 +7,21 @@ const db = require('../models');
 router.use(exposeUserInView);
 
 router.get('/', async (req, res) => {
-    const { Topic, Answerable, Sequelize } = db;
+    const { Topic, Answerable, Subscription, Sequelize } = db;
+    // hasn't gone through raven
+    if (!req.user) return res.redirect('/login');
 
     // note on the query below
     // i'm not sure entirely how it works
     // i spent a LONG time experimenting and it's weird
-    // especially how for group we use the model name of Topic and table name of Answerable...
+    // especially how for GROUP BY we use the model name of Topic and table name of Answerable...
+    // and use of "Subscriptions" when model name is Subscription and table name is subscriptions
+    // https://stackoverflow.com/questions/53566038/counting-relationships-in-many-to-many-table-between-joined-tables-in-sequelize
+    // https://bezkoder.com/sequelize-associate-many-to-many/
 
     const topics = await Topic.findAll({
         attributes: {
-            include: [[Sequelize.fn("COUNT", Sequelize.col("answerables.id")), "answerableCount"]]
+            include: [[Sequelize.fn("COUNT", Sequelize.col("answerables.id")), "answerableCount"], [Sequelize.fn("COUNT", Sequelize.col("Subscriptions.topicId")), "subCount"]]
         },
         include: [{
             model: Answerable,
@@ -25,10 +30,25 @@ router.get('/', async (req, res) => {
             through: {
                 attributes: [],
             }
+        }, {
+            model: Subscription,
+            attributes: [],
         }],
-        group: ['Topic.id', "answerables.id"]
-        
+        group: [['Topic.id', "answerables.id"], ["Topic.id"]],
+        raw: true
     });
+
+    // const topic = await Topic.findOne({ where: { name: "2P1_MECHANICS" } });
+    // console.log(req.user);
+
+    // const sub = await Subscription.create({
+    //     name: "hi",
+    //     topicId: topic.id,
+    //     repeatDayFrequency: 2,
+    //     count: 2,
+    //     repeatTime: new Date(),
+    //     userId: req.user.id
+    // });
 
     return res.render("topics", { title: "All topics", topics });
 });
--- app/routes/users.js
+++ app/routes/users.js
@@ -6,20 +6,24 @@ const router = express.Router();
 // make user available in view
 router.use(exposeUserInView);
 
-router.get('/home', async (req, res)=> {
+router.get('/home', async (req, res) => {
     const { User } = db;
     if (!(req.user instanceof User)) {
         return res.redirect('/signup');
     }
     const subscriptions = await req.user.getSubscriptions();
-    res.render("home", {
+    return res.render("home", {
         title: "Home",
         subscriptions
     });
 });
 
-router.get('/subscriptions', async (req, res)=> {
+router.get('/subscriptions', async (req, res) => {
     // if user doesn't give name, set it to the topic name
+    // hasn't gone through raven
+    if (!req.user) return res.redirect('/login');
+    res.send("Hi");
+
 });
 
 module.exports = router;
