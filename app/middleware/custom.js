0737fb4ebf775da3532c6c45b947cdbb0b47df5f
Matias Silva <mw781@srcf.net>
2021-02-27 11:39:08 +0000

    Add middleware for requiring auth

--- app/middleware/custom.js
+++ app/middleware/custom.js
@@ -1,8 +1,14 @@
 const { User } = require('../models');
 
 module.exports = {
+    // possible improvements here with combining these two methods
     exposeUserInView: function (req, res, next) {
         res.locals.user = (req.user instanceof User) && req.user;
+        next();
+    },
+    requireAuth: function(req, res, next){
+        // user has not logged in at all
+        if (!(req.user instanceof User)) return res.redirect('/login');
         next();
     }
 };
