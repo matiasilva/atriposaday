0d7e52814be4852c22646232e11f2936a5c40ead
Matias Silva <mw781@srcf.net>
2021-02-27 18:08:16 +0000

    Fix endless auth loop

--- app/middleware/custom.js
+++ app/middleware/custom.js
@@ -8,7 +8,11 @@ module.exports = {
     },
     requireAuth: function(req, res, next){
         // user has not logged in at all
-        if (!(req.user instanceof User)) return res.redirect('/login');
+        if(!req.user) return res.redirect('/login');
+        // user has valid crsid, BUT isn't in the DB
+        // we automatically deserialize user, so req.user will hold
+        // a valid user if they exist
+        if (!(req.user instanceof User)) return res.redirect('/signup');
         next();
     }
 };
