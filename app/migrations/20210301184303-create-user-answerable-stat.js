31ddcb0f45cc2ea62707b20f8a2c2cfa8e8c5a4e
Matias Silva <mw781@srcf.net>
2021-03-01 19:32:05 +0000

    Add primary ley to user stats table

--- app/migrations/20210301184303-create-user-answerable-stat.js
+++ app/migrations/20210301184303-create-user-answerable-stat.js
@@ -2,6 +2,12 @@
 module.exports = {
   up: async (queryInterface, {DataTypes}) => {
     await queryInterface.createTable('user_answerable_stats', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
       answerableId: {
         type: DataTypes.INTEGER,
         references: {
