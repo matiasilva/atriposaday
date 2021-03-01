64d927beb40691008fb78b646755b56b6d6ab259
Matias Silva <mw781@srcf.net>
2021-03-01 07:42:30 +0000

    Changed how boot exports env vars

--- app/scripts/boot.sh
+++ app/scripts/boot.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -e
 
+cd /path/to/app
+
+# read env vars from ecosystem file
+while read -r line; do
+    VAL=$(echo "$line" | sed -r 's/^([A-Za-z0-9_]+) *: *(.+)/export \1=\2/')
+    eval "$VAL"
+done < <(./yq e '.apps[0].env' ecosystem.yml)
+
 pm2 start ecosystem.yml
